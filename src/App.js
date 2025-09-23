aa7bfe82265884f0ea19c84174e36336df14c5e0
조상철 <chakanchul2533@gmail.com>
2025-09-23 23:10:45 +0900

    feat: #7.4 Movie App part Two1

--- src/App.js
+++ src/App.js
@@ -22,16 +22,8 @@ function App() {
     ) : (
       <div>
         {movies.map(movie => (
-          <div key={movie.id}>
-            <img src={movie.medium_cover_image} />
-            <h2>{movie.title}</h2>
-            <p>{movie.summary}</p>
-            <ul>
-              {movie.genres.map(g => (
-                <li key={g}>{g}</li>
-              ))}
-            </ul>
-          </div>))
+          <Movie coverImg={movie.medium_cover_image}/>
+          ))
         }
       </div>)}
   </div>
